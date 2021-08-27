46184bd1f6cafb1a33435c958d8c4a4b5b380414
KhCrv <pedusty@hotmail.com>
2021-08-27 17:35:53 +0700

    Rewrite

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
+const { Client, MessageEmbed }= require('discord.js');
 const puppeteer = require('puppeteer');
-const Discord = require("discord.js"); // npm i discord.js@12.5.3
-const client = new Discord.Client();
+const client = new Client();
+
+const config = {
+  token: "ํYour Token Here",
+  prefix: "/",
+  api_url: "http://lootranslator.infinityfreeapp.com/lootranslator.php"
+}
 
 client.on('ready', () => {
     console.log(`ล็อกอินสำเร็จ ${client.user.tag}! GEE`);
   });
 
-client.on('message', message => {
-    const args = message.content.trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+client.on('message', async (msg) => {
+    const args = msg.content.trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
 
-if(command == "/loo") {
-    (async () => {
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.goto('http://lootranslator.infinityfreeapp.com/lootranslator.php?text='+args[0]+'&mode=thai2loo&i=1')
-      const spanVal =  await page.$eval('body > div', el => el.innerText);
-      const embed = new Discord.MessageEmbed()
-        .setColor('#00ffff')
-        .setTitle(spanVal)
+    if (cmd == `${config.prefix}loo`) {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(`${config.api_url}?text=${args[0]}&mode=thai2loo&i=1`);
+      const res = await page.$eval('body > div', r => r.innerText);
+      const embed = new MessageEmbed()
+        .setTitle('Loo Translator')
+        .setColor('#00fff')
+        .setDescription(`\`${res}\``)
         .setFooter('Support By ⵝⵉⵏⵏⴻⵔⴿⵓⵏ');
-        message.channel.send(embed);
-      
+      msg.channel.send(embed)
       await browser.close()
-    })()
-  }
+    }
 });
-client.login("");
+
+client.login(config.token);
